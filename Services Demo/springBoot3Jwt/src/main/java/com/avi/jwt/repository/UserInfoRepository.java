3d36ec6dc8b9b83711ab5f0fe62c20b6df3cad7e
Avinash Upadhyay <avinash.upadhyay@in.ey.com>
2024-03-12 16:41:03 +0530

    SpringBoot 3 JWT

---
+++ Services Demo/springBoot3Jwt/src/main/java/com/avi/jwt/repository/UserInfoRepository.java
@@ -0,0 +1,11 @@
+package com.avi.jwt.repository;
+
+import com.avi.jwt.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByName(String username);
+
+}
---
+++ Services Demo/springBoot3Jwt/src/test/java/com/avi/jwt/SpringBoot3JwtApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.avi.jwt;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest(classes = SpringBoot3JwtApplicationTests.class)
+//class SpringBoot3JwtApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
